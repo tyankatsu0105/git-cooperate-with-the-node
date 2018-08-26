c2e97dc4cf113a511aa816fc852bcdbc9e674b40
山本 勝也 <frips.rysilufupe@gmail.com>
2018-08-26 13:21:22 +0900

    perf: :pill: prefixのruleをangular純正のものに変更
    extendsを入れてるのにprefix ruleをいじるのはおかしい

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,25 +3,6 @@ module.exports = {
   rules: {
     'subject-case': [0],
     'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [0],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-        'breaking',
-        'other'
-      ]
-    ]
+    'footer-leading-blank': [0]
   }
 };
